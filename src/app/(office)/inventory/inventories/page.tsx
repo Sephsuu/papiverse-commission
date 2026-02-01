c61829a98248ab6ddd06d637e8a14ac86a502640
Joseph Bataller <batallerjem208@gmail.com>
2026-02-01 09:59:11 +0800

    01/02/2026 - restrict access and fixed card groups at inventory summary

--- src/app/(office)/inventory/inventories/page.tsx
+++ src/app/(office)/inventory/inventories/page.tsx
@@ -1,6 +1,8 @@
 import { InventoriesPage } from "@/features/inventory/InventoriesPage";
+import { requireRole } from "@/lib/auth";
 
-export default function Inventory()  {
+export default async function Inventory()  {
+    await requireRole(['FRANCHISOR', 'FRANCHISEE']);
     return (
         <InventoriesPage />
     )
