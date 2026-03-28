7480b1072da6675e72992b53dd94eb6f86a54193
Sephsuu <batallerjem208@gmail.com>
2026-03-28 11:03:56 +0800

    ignore case

--- src/app/(office)/inventory/page.tsx
+++
@@ -1,9 +0,0 @@
-import { InventoryPage } from "@/features/_inventory-v2/InventoryPage";
-import { requireRole } from "@/lib/auth";
-
-export default async function Inventory() {
-    await requireRole(['FRANCHISOR']);
-    return (
-        <InventoryPage />
-    )
-}
